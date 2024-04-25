4e6bf40bb3d5d776d641915022d9fd08203511d1
jainam-b <jainambagrecha16@gmail.com>
2024-04-25 14:19:23 +0530

    angular

---
+++ angularDemo/src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      housing-location works!
+    </p>
+  `,
+  styleUrl: './housing-location.component.css'
+})
+export interface HousingLocation {
+  id: number;
+  name: string;
+  city: string;
+  state: string;
+  photo: string;
+  availableUnits: number;
+  wifi: boolean;
+  laundry: boolean;
+}
