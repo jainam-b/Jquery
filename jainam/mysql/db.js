87af55f659ccf7a12258fcb9574007dac58f926f
jainam-b <jainambagrecha16@gmail.com>
2024-05-07 17:30:56 +0530

    mysql updates

--- jainam/mysql/db.js
+++ jainam/mysql/db.js
@@ -1,11 +1,13 @@
+// import mysql module
 const mysql=require('mysql');
+// database info 
 const con=mysql.createConnection({
     host:"localhost",
     user:"root",
     password:"jainam",
     database:"AWS",
 });
-
+// connect with mysql 
 con.connect(function(err){
     if (err ) throw err;
     else 
